34faa9979ab000b4ca4958242978fcc06a7657fc
Abhijit <abhijit.khanna@live.com>
2017-10-01 18:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ function Objects() {
   // var recipes = {};
   var recipes = new Object();
 
-  return recipes;
+  // return recipes;
 }
 
 function updateObjectWithKeyAndValue (objectVar, keyVar, valueVar) {
 
+  var cloneObject = Object.assign({}, objectVar)
 
   objectVar[keyVar] = valueVar;
   return objectVar;
+
+  cloneObject = object.assign
 
 }
